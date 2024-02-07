003695062426e4f2dc3b5b34b6d5a4e4c5f057ea
GKPedrsen <GKPedrsen@gmail.com>
2024-02-07 20:24:09 +0100

    define AccessFlag enum and str map in FieldMethodStruct with accessor APIs

--- include/ClassFile/ClassFile.hpp
+++ include/ClassFile/ClassFile.hpp
@@ -13,6 +13,26 @@ struct FieldMethodInfo
   U16 NameIndex;
   U16 DescriptorIndex;
   std::vector< std::unique_ptr<AttributeInfo> > Attributes;
+
+  enum class AccessFlag : U16
+  {
+    PUBLIC       = 0x0001,
+    PRIVATE      = 0x0002,
+    PROTECTED    = 0x0004,
+    STATIC       = 0x0008,
+    FINAL        = 0x0010,
+    SYNCHRONIZED = 0x0020,
+    BRIDGE       = 0x0040,
+    VARARGS      = 0x0080,
+    NATIVE       = 0x0100,
+    ABSTRACT     = 0x0400,
+    STRICT       = 0x0800,
+    SYNTHETIC    = 0x1000,
+  };
+
+  static const std::unordered_map<AccessFlag, std::string_view> FlagStrMap;
+  static ErrorOr<std::string_view> FlagToStr(U16 flag);
+  std::vector<std::string_view> FlagsToStrs() const;
 };
 
 struct ClassFile 
---
+++ src/ClassFile.cpp
@@ -0,0 +1,47 @@
+#include "ClassFile/ClassFile.hpp"
+
+using namespace ClassFile;
+
+const std::unordered_map<FieldMethodInfo::AccessFlag, std::string_view> 
+FieldMethodInfo::FlagStrMap = 
+{
+#define FOO_TUPLE(X) {FieldMethodInfo::AccessFlag::X, #X}
+  FOO_TUPLE(PUBLIC),
+  FOO_TUPLE(PRIVATE),
+  FOO_TUPLE(PROTECTED),
+  FOO_TUPLE(STATIC),
+  FOO_TUPLE(FINAL),
+  FOO_TUPLE(SYNCHRONIZED),
+  FOO_TUPLE(BRIDGE),
+  FOO_TUPLE(VARARGS),
+  FOO_TUPLE(NATIVE),
+  FOO_TUPLE(ABSTRACT),
+  FOO_TUPLE(STRICT),
+  FOO_TUPLE(SYNTHETIC),
+#undef FOO_TUPLE
+};
+
+ErrorOr<std::string_view> FieldMethodInfo::FlagToStr(U16 flag)
+{
+  auto it = FieldMethodInfo::FlagStrMap.find(static_cast<AccessFlag>(flag));
+
+  if(it != FieldMethodInfo::FlagStrMap.end())
+    return it->second;
+
+  return Error{"FieldMethodInfo::FlagToStr(): undefined flag value."};
+}
+
+std::vector<std::string_view> FieldMethodInfo::FlagsToStrs() const
+{
+  std::vector<std::string_view> flags;
+
+  for(const auto& it : FieldMethodInfo::FlagStrMap)
+  {
+    if(this->AccessFlags & static_cast<U16>(it.first))
+      flags.push_back(it.second);
+  }
+
+  return flags;
+}
+
+
