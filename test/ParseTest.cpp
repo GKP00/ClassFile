2f1876e0860b5d2815d2662425598f39a330b293
GKPedrsen <GKPedrsen@gmail.com>
2024-04-16 12:19:16 +0200

    add basic parse test for complex class file
    the class file is taken from a real obfuscated java game client

--- test/ParseTest.cpp
+++ test/ParseTest.cpp
@@ -7,41 +7,51 @@
 #include <fstream>
 #include <iostream>
 
+#ifndef RES_DIR
+  #define RES_DIR "res"
+#endif
+
 class FileParseTest : public ::testing::Test
 {
   protected:
     ClassFile::ErrorOr<ClassFile::ClassFile> errOrSimpleClass{
       ClassFile::Error{"ErrOr uninitialized in test"}};
 
+    ClassFile::ErrorOr<ClassFile::ClassFile> errOrComplexClass{
+      ClassFile::Error{"ErrOr uninitialized in test"}};
+
     void SetUp() override
     {
-      #ifdef RES_DIR
-      const char* filePath = RES_DIR"/HelloWorld.class";
-      #else
-      const char* filePath = "./res/HelloWorld.class";
-      #endif
-
       try
       {
-        std::ifstream is{filePath};
+        std::ifstream is{RES_DIR"/HelloWorld.class"};
         is.exceptions(std::ifstream::failbit | std::ifstream::badbit);
 
         errOrSimpleClass = std::move(ClassFile::Parser::ParseClassFile(is));
+        is.close();
+
+        is.open(RES_DIR"/Complex.class");
+        errOrComplexClass = std::move(ClassFile::Parser::ParseClassFile(is));
       }
       catch(std::exception& e)
       {
-        FAIL() << "stream threw while reading from \"" 
-               << filePath
-               << "\" (probably failed to open). Exception.what(): " 
+        FAIL() << "Setup failed: failed to read test files: \n"
                << e.what();
       }
     }
 
 };
 
-TEST_F(FileParseTest, SuccessfulParse)
+TEST_F(FileParseTest, SuccessfulParseSimple)
 {
   ASSERT_TRUE( !errOrSimpleClass.IsError() )
     << "Parsing failed for simple valid class, error:\n" 
     << errOrSimpleClass.GetError().What;
+}
+
+TEST_F(FileParseTest, SuccessfulParseComplex)
+{
+  ASSERT_TRUE( !errOrComplexClass.IsError() )
+    << "Parsing failed for complex valid class, error:\n" 
+    << errOrComplexClass.GetError().What;
 }
