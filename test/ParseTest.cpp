3b72a26e820281ed082af2a653d3c63c764595c0
GKPedrsen <GKPedrsen@gmail.com>
2024-06-01 04:27:32 +0200

    add check for constant value in wide instruction parse test

--- test/ParseTest.cpp
+++ test/ParseTest.cpp
@@ -55,6 +55,34 @@ TEST_F(FileParseTest, SuccessfulParseWideInstr)
   ASSERT_TRUE( !errOrWideClass.IsError() )
     << "Parsing failed for class with wide instr, error:\n" 
     << errOrSimpleClass.GetError().What;
+
+  ClassFile::ClassFile cf = errOrWideClass.Release();
+
+  //find code
+  ClassFile::CodeAttribute* pCode;
+  for(const auto& method : cf.Methods)
+  {
+    auto codeItr = 
+      std::find_if(method.Attributes.begin(), method.Attributes.end(), 
+          [](const auto& pAttr) 
+          { return pAttr->GetType() == ClassFile::AttributeInfo::Type::Code;});
+
+    pCode = codeItr == method.Attributes.end() ? nullptr :
+      static_cast<ClassFile::CodeAttribute*>((*codeItr).get());
+  }
+
+  ASSERT_TRUE(pCode != nullptr);
+
+  //find the wide instr
+  auto wideInstr = 
+    std::find_if(pCode->Code.begin(), pCode->Code.end(), 
+        [](const auto& instr){ return instr.IsWide(); });
+
+  ASSERT_TRUE(wideInstr != pCode->Code.end());
+
+  //test the increment constant was successfully parsed as 128
+  auto constant = (*wideInstr).GetOperand(1).GetOrElse(0);
+  ASSERT_TRUE(constant == 128);
 }
 
 TEST_F(FileParseTest, SuccessfulParseSimple)
